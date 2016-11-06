53b7d6a83761edc904c2245dc62f940cf387a8b6
Casey Platt <kse0824@gmail.com>
2016-11-06 14:45:09 -0600

    3 quick practice loops

---
+++ 3quickies.rb
@@ -0,0 +1,37 @@
+
+
+names = ["Peter","Bob", "Mary"]
+b = []
+
+names.each do |name|
+	 name = "Hello " + name  
+	 b << name
+     end
+
+puts b
+
+
+
+numbers = [3, 8, 7]
+e = []
+	
+numbers.each do |number|
+	number = number * 2
+	e << number
+    end
+
+puts e
+
+
+
+words = ["hello", "goodbye", "Peter"]
+q = []
+
+words.each do |word|
+	word = word.chr
+      q << word
+  	end
+
+puts q
+
+
